7213717c04524ffc8ac2e77a95924a39dbc40b8b
Austin Dulay <dulayaustin@gmail.com>
2024-05-20 00:16:00 +0800

    Setup Email structure

--- app/models/applicant.rb
+++ app/models/applicant.rb
@@ -25,6 +25,8 @@ class Applicant < ApplicationRecord
 
   belongs_to :job
 
+  has_many :emails, dependent: :destroy
+
   validates_presence_of :first_name, :last_name, :email
 
   FILTER_PARAMS = %i[ query job sort].freeze
--- config/routes.rb
+++ config/routes.rb
@@ -32,7 +32,7 @@ Rails.application.routes.draw do
     member do
       patch :change_stage
     end
-
+    resources :emails, only: %i[ index new create show ]
     get :resume, action: :show, controller: "resumes"
   end
 end
