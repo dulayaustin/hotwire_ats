0804ed3854f6233f2d00f527d4c13fad437b2df9
Austin Dulay <dulayaustin@gmail.com>
2024-05-20 21:38:39 +0800

    Display emails index and show actions

--- app/controllers/emails_controller.rb
+++ app/controllers/emails_controller.rb
@@ -1,6 +1,18 @@
 class EmailsController < ApplicationController
   before_action :authenticate_user!
   before_action :set_applicant
+  before_action :set_email, only: %i[ show ]
+
+  def index
+    @emails = Email.where(applicant_id: @applicant.id).with_rich_text_body.order(created_at: :asc)
+  end
+
+  def show
+    html = render_to_string(partial: 'emails/email', locals: { email: @email } )
+    render cable_ready: cable_car
+      .inner_html('#slideover-content', html: html)
+      .text_content('#slideover-header', text: @email.subject)
+  end
 
   def new
     @email = Email.new
@@ -29,6 +41,10 @@ class EmailsController < ApplicationController
   end
 
   private
+
+    def set_email
+      @email = Email.find(params[:id])
+    end
 
     def set_applicant
       @applicant = Applicant.find(params[:applicant_id])
