1c7c0258507a11133e0f89925efff69669db2eb6
Austin Dulay <dulayaustin@gmail.com>
2024-05-27 23:14:46 +0800

    Filter hiring stage Pie chart

--- app/models/charts/hiring_stages_chart.rb
+++ app/models/charts/hiring_stages_chart.rb
@@ -1,17 +1,21 @@
 class Charts::HiringStagesChart
-  def initialize(account_id)
+  def initialize(account_id, job_id = nil)
     @account_id = account_id
+    @job_id = job_id
   end
 
   def generate
     query_data
   end
 
-  def query_data
-    Applicant
-      .includes(:job)
-      .for_account(@account_id)
-      .group('stage')
-      .count
-  end
+  private
+
+    def query_data
+      Applicant
+        .includes(:job)
+        .for_account(@account_id)
+        .for_job(@job_id)
+        .group('stage')
+        .count
+    end
 end
