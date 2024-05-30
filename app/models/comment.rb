d8afee7a7411d53d9cb9047884422f9c35ba3279
Austin Dulay <dulayaustin@gmail.com>
2024-05-30 22:38:59 +0800

    Notify mentioned users

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -3,4 +3,22 @@ class Comment < ApplicationRecord
   belongs_to :commentable, polymorphic: true, counter_cache: :commentable_count, touch: true
 
   has_rich_text :comment
+
+  after_create_commit :notify_mentioned_users
+
+  def notify_mentioned_users
+    mentioned_users.each do |mentioned_user|
+      ApplicantCommentNotification.create(
+        user: mentioned_user,
+        params: {
+          user: user,
+          applicant: commentable
+        }
+      )
+    end
+  end
+
+  def mentioned_users
+    comment.body.attachments.select { |att| att.attachable.is_a?(User) }.map(&:attachable).uniq
+  end
 end
